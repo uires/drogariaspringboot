146602d6966df6d3d156885c5ba5767bd48e748e
uires <uiresdeivide1@gmail.com>
2018-06-25 15:01:31 -0300

    criando dataSource

--- src/main/java/br/com/drogaria/Configuracao.java
+++ src/main/java/br/com/drogaria/Configuracao.java
@@ -2,9 +2,12 @@ package br.com.drogaria;
 
 import javax.sql.DataSource;
 
+import org.hibernate.engine.jdbc.connections.internal.DriverManagerConnectionCreator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class Configuracao {
@@ -15,6 +18,11 @@ public class Configuracao {
 	
 	@Bean
 	public DataSource data() {
-		return null;
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUrl("jdbc:mysql://localhost:8080:3306/drogaria");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUsername("root");
+		dataSource.setPassword("220132");
+		return dataSource;
 	}
 }
