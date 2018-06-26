346852b54519faaa9569710b686af67eb3b627aa
uires <uiresdeivide1@gmail.com>
2018-06-25 21:51:02 -0300

    Data Source para a persistencia

--- src/main/java/br/com/drogaria/Configuracao.java
+++ src/main/java/br/com/drogaria/Configuracao.java
@@ -1,13 +1,26 @@
 package br.com.drogaria;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class Configuracao {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
 	}
-	
+
+	@Bean
+	public DataSource data() {
+		DriverManagerDataSource data = new DriverManagerDataSource();
+		data.setDriverClassName("com.mysql.jdbc.Driver");
+		data.setUrl("jdbc:mysql://localhost:3306/drogaria");
+		data.setUsername("root");
+		data.setPassword("220132");
+		return data;
+	}
 }
