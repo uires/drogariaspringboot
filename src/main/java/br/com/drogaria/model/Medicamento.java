6587d442f3d598807f331002591ac87581053cd7
uires <uiresdeivide1@gmail.com>
2018-06-25 21:55:21 -0300

    domain para mapamento no banco de dados com hibernate

---
+++ src/main/java/br/com/drogaria/model/Medicamento.java
@@ -0,0 +1,68 @@
+package br.com.drogaria.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "medicamentos")
+
+public class Medicamento {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nome;
+	private int quantidade;
+	private double preco;
+	private String substancias;
+	private double pesoLiquido;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getSubstancias() {
+		return substancias;
+	}
+
+	public void setSubstancias(String substancias) {
+		this.substancias = substancias;
+	}
+
+	public double getPesoLiquido() {
+		return pesoLiquido;
+	}
+
+	public void setPesoLiquido(double pesoLiquido) {
+		this.pesoLiquido = pesoLiquido;
+	}
+
+}
