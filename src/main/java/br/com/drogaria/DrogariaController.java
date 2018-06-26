d9dadfe8f87c61f834d1f104afb39bc366e10d2f
uires <uiresdeivide1@gmail.com>
2018-06-26 16:21:56 -0300

    mapeando parametros para entidade

--- src/main/java/br/com/drogaria/DrogariaController.java
+++ src/main/java/br/com/drogaria/DrogariaController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.drogaria.model.Medicamento;
 import br.com.drogaria.repository.MedicamentoRepository;
@@ -22,6 +24,12 @@ public class DrogariaController {
 	public String gereciamentoPage(Model modelo) {
 		Iterable<Medicamento> medicamentos = repository.findAll();
 		modelo.addAttribute("medicamentos", medicamentos);
+		return "gerenciamento-medicamento";
+	}
+	
+	@RequestMapping(value = "cadastrar", method = RequestMethod.POST)
+	public String cadastraMedicamento(@RequestParam("nome") String nome) {
+		
 		return "gerenciamento-medicamento";
 	}
 	
