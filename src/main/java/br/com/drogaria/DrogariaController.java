137fcccd98b075bd88b713863e275e2d0646ee16
uires <uiresdeivide1@gmail.com>
2018-06-25 21:59:15 -0300

    exclusao classe

--- src/main/java/br/com/drogaria/DrogariaController.java
+++ src/main/java/br/com/drogaria/DrogariaController.java
@@ -1,18 +1,28 @@
 package br.com.drogaria;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.drogaria.model.Medicamento;
+import br.com.drogaria.repository.MedicamentoRepository;
 
 @Controller
 public class DrogariaController {
-
+	@Autowired
+	private MedicamentoRepository repository;
+	
 	@RequestMapping("/")
 	public String indexPage() {
 		return "index";
 	}
 
 	@RequestMapping("gerenciamento-medicamento")
-	public String gereciamentoPage() {
+	public String gereciamentoPage(Model modelo) {
+		Iterable<Medicamento> medicamentos = repository.findAll();
+		
 		return "gerenciamento-medicamento";
 	}
+	
 }
